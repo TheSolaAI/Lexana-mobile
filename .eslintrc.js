06386b16d88c14f6c79bcfe93fee21d5dc132ca9
Guhan Sensam <guhansensam@gmail.com>
2025-04-17 16:58:22 +0530

    chore: :wrench: Added Dev Configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,56 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
-    // `expo` must come after `standard` or its globals configuration will be overridden
-    "expo",
-    // `jsx-runtime` must come after `expo` or it will be overridden
-    "plugin:react/jsx-runtime",
-    "prettier",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'expo',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
-  plugins: ["reactotron", "prettier"],
+  plugins: ['reactotron', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
     // typescript-eslint
-    "@typescript-eslint/array-type": 0,
-    "@typescript-eslint/ban-ts-comment": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    '@typescript-eslint/array-type': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-require-imports": 0,
-    "@typescript-eslint/no-empty-object-type": 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-require-imports': 0,
+    '@typescript-eslint/no-empty-object-type': 0,
     // eslint
-    "no-use-before-define": 0,
-    "no-restricted-imports": [
-      "error",
+    'no-use-before-define': 0,
+    'no-restricted-imports': [
+      'error',
       {
         paths: [
           // Prefer named exports from 'react' instead of importing `React`
           {
-            name: "react",
-            importNames: ["default"],
+            name: 'react',
+            importNames: ['default'],
             message: "Import named exports from 'react' instead.",
           },
         ],
       },
     ],
     // react
-    "react/prop-types": 0,
+    'react/prop-types': 0,
     // react-native
-    "react-native/no-raw-text": 0,
+    'react-native/no-raw-text': 1,
+    'react-native/no-inline-styles': 0,
     // reactotron
-    "reactotron/no-tron-in-production": "error",
+    'reactotron/no-tron-in-production': 'error',
     // eslint-config-standard overrides
-    "comma-dangle": 0,
-    "no-global-assign": 0,
-    "quotes": 0,
-    "space-before-function-paren": 0,
+    'comma-dangle': 0,
+    'no-global-assign': 0,
+    quotes: 0,
+    'space-before-function-paren': 0,
   },
-}
+};
